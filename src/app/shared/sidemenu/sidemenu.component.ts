a22def213f35c7b584f5fc4b61fb4de0b679a783
Carlos Eduardo <caeca425@gmail.com>
2025-02-05 21:42:56 -0500

    Creación del sidemenu y mapeo de las rutas desde el sidemenu.component.ts

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
@@ -11,4 +12,25 @@ import { Component } from '@angular/core';
     }
   `,
 })
-export class SidemenuComponent { }
+export class SidemenuComponent {
+
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    //el .flat organiza un arreglo y lo aplana un poco, o sea que muestra los valores dentro de los arreglos
+    .flat()
+    .filter( route => route && route.path ) //validamos si la ruta existe y si tiene path lo colocamos en el dashBoardRoutes
+    .filter( route => !route.path?.includes(':')); //con este validamos que no nos traiga ninguna ruta con ":" o sea que con esto retiramos el path user/:id
+
+
+    constructor(){
+    // const dashBoardRoutes = routes
+    // .map(route => route.children ?? [])
+    // //el .flat organiza un arreglo y lo aplana un poco, o sea que muestra los valores dentro de los arreglos
+    // .flat()
+    // .filter( route => route && route.path ) //validamos si la ruta existe y si tiene path lo colocamos en el dashBoardRoutes
+    // .filter( route => !route.path?.includes(':')) //con este validamos que no nos traiga ninguna ruta con ":" o sea que con esto retiramos el path user/:id
+
+    // console.log(dashBoardRoutes);
+  }
+
+ }
