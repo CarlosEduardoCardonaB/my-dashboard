9f1cd017b3f4105e0ea43dd1e7649f10634227e6
Carlos Eduardo <caeca425@gmail.com>
2025-02-14 22:33:45 -0500

    se trabaja en los defer views, para cargas de páginas pesada y mostrar el loadign mientras carga la página

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,16 +1,54 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from "../../../shared/title/title.component";
+import { JsonPipe } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: `
-    :host {
-      display: block;
-    }
+  imports: [TitleComponent, JsonPipe],
+  template: `<app-title [title]="currentFramework()" />
+
+    <pre>{{ frameWorkAsSignal() | json}}</pre>
+    <pre>{{ frameWorkAsProperty | json }}</pre>
+
   `,
+  //El changeDetection nos sirve para hacer la verificación de un cambio puntual y no estar pendiente de cambios de manera global en el proyecto.
+  //Esto son optimizaciones de Angular para performance, de la manera anterior Angular está escuchando cambios en todo el proyecto y esto castiga performance
+  //Entonces la idea es trabajar con señales(signals) para mejorar performance
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 //Con la palabra "default" podemos usar la ruta directa a este componente en el app.routes.ts
-export default class ChangeDetectionComponent { }
+export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => `Change detection - ${ this.frameWorkAsSignal().name }`
+  );
+
+  public frameWorkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  });
+
+  public frameWorkAsProperty = ({
+    name: 'Angular',
+    releaseDate: 2016
+  });
+
+
+  constructor(){
+    setTimeout(() => {
+
+      //Forma tradicional de escuchar cambios, angular cambiará en tiempo real el valor visual pero esto puede ser deprecado por no ser optimo para el rendimiento
+      this.frameWorkAsProperty.name = 'React';
+
+      //Forma recomendada con señales
+      this.frameWorkAsSignal.update( value => {
+        value.name = 'React';
+        return {...value};
+      });
+
+      console.log('Cargado constructor');
+    }, 3000);
+  }
+
+ }
--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,15 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
-  template: `<p>heavy-loaders-slow works!</p>`,
-  styles: `
-    :host {
-      display: block;
-    }
+  imports: [CommonModule],
+  template: `
+  @if (visible) {
+    <section [ngClass]="['w-full h-[600px]', cssClass]" visible>
+      Heavy Loader Slow
+    </section>
+  }
+
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+
+
 })
-export class HeavyLoadersSlowComponent { }
+export default class HeavyLoadersSlowComponent {
+
+  public visible: boolean = false;
+
+  @Input({ required: true }) cssClass!: string;
+
+
+  constructor(){
+
+    //Esto sirve para cargar un poco el componente y alcanzar a ver el defer
+    for(var i=0; i < 20000; i++){
+        if(i == 19999){
+          this.visible = true;
+        }
+        if( i % 20 == 0){
+          console.log(i);
+        }
+    }
+
+    // setTimeout(() => {
+    //   this.visible = true;
+    // }, 3000);
+    console.log('Heavy loader component');
+  }
+ }
